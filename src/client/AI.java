d33caa681ac538d871ef73e8ad36778d6a2f0ceb
aminjoon <maminbajand@yahoo.com>
2016-02-13 19:47:18 +0330

    Supporting & Expanding
    Nodes Under Attack roles has major problem . need to solve this.

--- src/client/AI.java
+++ src/client/AI.java
@@ -3,6 +3,7 @@ package client;
 import client.model.Node;
 import com.sun.org.apache.xpath.internal.operations.Bool;
 
+import javax.swing.text.StyledEditorKit;
 import java.lang.reflect.Array;
 import java.util.*;
 import java.util.function.BooleanSupplier;
@@ -18,13 +19,22 @@ import java.util.function.BooleanSupplier;
  * See World interface for more details.
  */
 public class AI {
-    private Map<Node, Node> currentMoveList = new HashMap<Node, Node>(); //<source,destination>
+    private Map<Integer, Integer> previousMoveList = new HashMap<Integer, Integer>();
+    private Map<Integer, Integer> currentMoveList = new HashMap<Integer, Integer>(); //<source,destination>
+
+    private List<Node> supporter = new ArrayList<Node>();
+    private List<Node> underAttack = new ArrayList<Node>();
     private List<Node> expandingCondidate = new ArrayList<Node>();
-    private Map<Node,Boolean> changeCondidate = new HashMap<Node, Boolean>();
+    private Map<Node, Boolean> changeCondidate = new HashMap<Node, Boolean>();
     private Boolean isNodeHasFreeNeighbour = false;
+    private Boolean isNodeHasEnemyNeighbour = false;
+
+    private List<Integer> enemyCoreNode = new ArrayList<Integer>();
 
     private int countNeighboursO1;
     private int countNeighboursO2;
+
+    private int enemyID;
 
     public void doTurn(World world) {
 //        initialize values
@@ -36,30 +46,52 @@ public class AI {
             for (Node node : nodes) {
                 expandingCondidate.add(node);
             }
+            Node[] nodes1 = world.getOpponentNodes();
+            for (Node node1 : nodes) {
+                enemyCoreNode.add(node1.getIndex());
+            }
+//            set enemy ID
+            if (world.getMyID() == 1) {
+                enemyID = 0;
+            } else {
+                enemyID = 1;
+            }
         }
 
-//        iteration
+/*
+#################################
+---------Expanding Part----------
+---------------------------------
+#################################
+ */
         for (Node source : expandingCondidate) {
             Node[] neighbours = source.getNeighbours();
 
             isNodeHasFreeNeighbour = false;
+            isNodeHasEnemyNeighbour = false;
             for (Node node : neighbours) {
                 if (node.getOwner() == -1)
                     isNodeHasFreeNeighbour = true;
+                else if (node.getOwner() != world.getMyID())
+                    isNodeHasEnemyNeighbour = true;
             }
             if (!isNodeHasFreeNeighbour) {
-                changeCondidate.put(source,false);
+                if (isNodeHasEnemyNeighbour) {
+                    underAttack.add(source);
+                } else {
+                    supporter.add(source);
+                }
+                changeCondidate.put(source, false);
                 continue;
             }
-//
-////            sorting
+
             Arrays.sort(neighbours, (o1, o2) -> {
                 if (o1.getOwner() == -1 && o2.getOwner() == -1) {
 
 //                    barrasi inke kase dige nakhad biad rooye in node
 
-                    for (Node node : currentMoveList.keySet()) {
-                        if (o1.getIndex() == currentMoveList.get(node).getIndex()) {
+                    for (int node : currentMoveList.keySet()) {
+                        if (o1.getIndex() == currentMoveList.get(node)) {
                             return 1;
                         }
                     }
@@ -80,7 +112,24 @@ public class AI {
 
                     return 1;
                 }
-                if (o1.getOwner() == -1 && o2.getOwner() != -1) {
+                if (o1.getOwner() == -1) {
+                    return -1;
+                }
+//routing to enemy core
+                if (o1.getOwner() == world.getMyID() && o2.getOwner() == world.getMyID()) {
+                    for (int node : previousMoveList.keySet()) {
+                        if (node == o1.getIndex() && previousMoveList.get(node) == source.getIndex()) {
+                            return 1;
+                        }
+                    }
+                    if (Math.abs(o1.getIndex() - enemyCoreNode.get(0)) < Math.abs(o2.getIndex() - enemyCoreNode.get(0))) {
+                        return -1;
+                    } else {
+                        return 1;
+                    }
+                }
+
+                if (o1.getOwner() == enemyID) {
                     return -1;
                 }
                 return 1;
@@ -89,18 +138,122 @@ public class AI {
             Node destination = neighbours[0];
             if (neighbours.length > 0) {
                 world.moveArmy(source, destination, source.getArmyCount() / 2);
-                currentMoveList.put(source, destination);
-                changeCondidate.put(destination,true);
+                currentMoveList.put(source.getIndex(), destination.getIndex());
+                changeCondidate.put(destination, true);
             }
         }
 //        update change candidate
-        for(Node node:changeCondidate.keySet()){
-            if(changeCondidate.get(node)){
+        for (Node node : changeCondidate.keySet()) {
+            if (changeCondidate.get(node)) {
                 expandingCondidate.add(node);
-            }else {
+            } else {
                 expandingCondidate.remove(node);
             }
         }
+        changeCondidate.clear();
+/*
+#################################
+---------Supporting Part----------
+---------------------------------
+#################################
+*/
+//        check under attack nodes
+//      brarye har nodi ke taht hamle ast , man khode node ra barraye inke tahte hamle hast ye na barrasi mikonam
+//      tamam node haye hamsaye'e ke tahte hamle hastand niz check khahad shod
+
+
+        System.out.println("###################################");
+        for (Node node : underAttack) {
+            System.out.println(node.getIndex());
+
+            if (node.getOwner() == enemyID) {
+                changeCondidate.put(node, false);
+                continue;
+            }
+            isNodeHasEnemyNeighbour = false;
+            for (Node node1 : node.getNeighbours()) {
+                if (node1.getOwner() == enemyID) {
+                    continue;
+                }
+                if (node1.getOwner() == world.getMyID()) {
+                    for (Node node2 : node1.getNeighbours()) {
+                        if (node2.getOwner() == enemyID) {
+                            if (!underAttack.contains(node1)) {
+                                changeCondidate.put(node1, true);
+                            }
+                            break;
+                        }
+                    }
+                } else if (node1.getOwner() == enemyID) {
+                    isNodeHasEnemyNeighbour = true;
+                }
+            }
+            if (!isNodeHasEnemyNeighbour) {
+                changeCondidate.put(node, false);
+            }
+        }
+
+//        update list
+        for (Node node : changeCondidate.keySet()) {
+            if (changeCondidate.get(node)) {
+                underAttack.add(node);
+            } else {
+                underAttack.remove(node);
+            }
+        }
+        changeCondidate.clear();
+
+
+//        taghsim bandi mikonam tedad nirohaee ke support hastan va tedad niroohaee ke tahte hamle hastand
+//        va be har tahte hamle ye tedad niro miroosanam
+        int underAttackCount = underAttack.size();
+        int supporterCount = supporter.size();
+        int counter = 0;
+        Boolean nodeMoved = false;
+        if (underAttackCount != 0) {
+            counter = supporterCount / underAttackCount;
+        }
+        for (int i = 0; i < underAttackCount; i++) {
+            Node node = this.underAttack.get(i);
+            for (int j = i * counter; j < counter; j++) {
+                nodeMoved = false;
+                Node[] neighbours = supporter.get(j).getNeighbours();
+                for (Node node1 :neighbours){
+                    if(node1.getIndex() == node.getIndex()){
+                        world.moveArmy(supporter.get(j), node, supporter.get(j).getArmyCount() * 2 / 3);
+                        nodeMoved = true;
+                        break;
+                    }
+                }
+                if(!nodeMoved){
+                    if (node.getIndex() > supporter.get(j).getIndex()) {
+                        Arrays.sort(neighbours, (o1, o2) -> {
+                            if (o1.getIndex() > o2.getIndex()) {
+                                return -1;
+                            }
+                            return 1;
+                        });
+                    } else {
+                        Arrays.sort(neighbours, (o1, o2) -> {
+                            if (o1.getIndex() < o2.getIndex()) {
+                                return -1;
+                            }
+                            return 1;
+                        });
+                    }
+                    world.moveArmy(supporter.get(j), neighbours[0], supporter.get(j).getArmyCount() * 2 / 3);
+                }
+            }
+        }
+
+/*
+#################################
+---------Attack Part----------
+---------------------------------
+#################################
+*/
+
+
     }
 
 
